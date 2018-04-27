7ee8578198ec2a8df0889308f7e74b84d5209edf
HyechurnJang <hyjang@cisco.com>
2018-04-27 15:14:40 +0900

    update

--- setenv.py
+++ setenv.py
@@ -19,21 +19,14 @@ if __name__ == '__main__':
     idx = 1
     for name, desc in networks.items():
         low_name = name.lower()
-        print 1
         os.system('ip netns add %s' % low_name)
-        print 2
         os.system('ip link add %s0 type veth peer name %s0 netns %s' % (low_name, low_name, low_name))
-        print 3
+        os.system('ifconfig %s0 up' % low_name)
         os.system('ip netns exec %s ifconfig %s0 1.254.1.%d netmask 255.255.255.0 up' % (low_name, low_name, idx))
-        print 4
         os.system('ip netns exec %s ifconfig lo up' % low_name)
-        print 5
         os.system('brctl addif rdhcp %s0' % low_name)
-        print 6
         os.system('ip link set %s netns %s' % (desc['interface'], low_name))
-        print 7
         os.system('ip netns exec %s ifconfig %s %s netmask %s up' % (low_name, desc['interface'], desc['address'], desc['netmask']))
-        print 8
         os.system('ip netns exec %s route add default gw %s' % (low_name, desc['gateway']))
         os.system('ip netns exec %s iptables -t nat -A POSTROUTING -j MASQUERADE' % low_name)
         os.system('ip netns exec %s iptables -t nat -A PREROUTING -p tcp -i %s --dport 8080 -j DNAT --to 1.254.1.254:8080' % (low_name, desc['interface']))
