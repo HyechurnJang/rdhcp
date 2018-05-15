164158b68fdffa86c645762acb04c40c879ef8c7
HyechurnJang <hyjang@cisco.com>
2018-05-15 18:12:38 +0900

    install

--- install.sh
+++ install.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+echo "Package Install"
 apt update
 apt install python-pip mariadb-server libmysqlclient-dev dnsmasq ntp -y
 pip install pygics ipaddress netifaces
+echo ""
 
+echo "System Setting"
+sed -i -e "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward/g" /etc/sysctl.conf
+sysctl -p
 systemctl disable dnsmasq
 systemctl stop dnsmasq
 systemctl enable ntp
 systemctl restart ntp
+cp xtra/bin/rdhcp /usr/bin/rdhcp
+chmod 755 /usr/bin/rdhcp
+echo ""
 
+echo "Database Initialization"
 mysql_secure_installation
 systemctl enable mysql
 systemctl restart mysql
+echo ""
 
